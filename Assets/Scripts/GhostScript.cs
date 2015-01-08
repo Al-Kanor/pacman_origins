0cb4b5ba66b2b9448516fc1267c4971cad5035d5
Mathieu Bédu <mathieu.bedu@hotmail.fr>
2015-01-08 14:04:05 +0100

    Base du projet

---
+++ Assets/Scripts/GhostScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GhostScript : MonoBehaviour {
+
+    public Transform goal;
+
+    private NavMeshAgent navComponent;
+
+    void Start () {
+        navComponent = this.gameObject.GetComponent<NavMeshAgent> ();
+    }
+
+    void FixedUpdate () {
+        if (goal) {
+            navComponent.SetDestination (goal.position);
+        }
+    }
+}
