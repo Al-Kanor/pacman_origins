278d7a04eb0e4ce7240f40b938ca7a10d71fbaf5
Adrien Peyromaure Debord Broca <a.peyromauredebordbroca@rubika-edu.com>
2015-01-15 12:27:24 +0100

    Updates GridManager (meal time)

--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -1,15 +1,51 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Tile : MonoBehaviour {
+public enum TILE_TYPES {
+    WALL = 0,
+    CORRIDOR,
+    GOAL,
+    SPAWN,
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    COUNT
+}
+
+public class Tile {
+    #region Members
+    private Vector2 m_Position;
+    private TILE_TYPES m_Type;
+    public TILE_TYPES p_Type {
+        get {
+            return m_Type;
+        }
+        set {
+            int valueToInt = (int)value;
+            if (valueToInt < 0
+                || valueToInt > (int)TILE_TYPES.COUNT) {
+                m_Type = TILE_TYPES.WALL;
+            }
+            else {
+                m_Type = value;
+            }
+        }
+    }
+    #endregion
+
+    #region Constructors
+    public Tile (float x, float y) {
+        m_Position = new Vector2 (x, y);
+    }
+
+    public Tile ()
+        : this (0.0f, 0.0f) { }
+    #endregion
+
+    public Vector2 GetPosition () {
+        return m_Position;
+    }
+
+    public void SetPosition (float x, float y) {
+        m_Position.x = x;
+        m_Position.y = y;
+    }
 }
--- Assets/Scripts/TileGridScript.cs
+++ Assets/Scripts/TileGridScript.cs
@@ -3,14 +3,162 @@ using System.Collections;
 
 public class TileGridScript : MonoBehaviour {
     #region Members
-
+    public Tile[][] m_Grid;
+    public const int c_GridWidth = 28;
+    public const int c_GridHeight = 31;
     #endregion
 
-    void Start () {
+    protected void Awake () {
+        InitializeGrid ();
+    }
+
+    protected void Update () {
 
     }
 
-    void Update () {
+    protected void InitializeGrid () {
+        m_Grid = new Tile[c_GridWidth][];
+        for (int i = 0; i < c_GridWidth; ++i) {
+            m_Grid[i] = new Tile[c_GridHeight];
+        }
 
+        float currentX = -13.5f;
+        for (int i = 0; i < c_GridWidth; ++i) {
+            float currentY = -15.0f;
+            for (int j = 0; j < c_GridHeight; ++j) {
+                m_Grid[i][j] = new Tile (currentX, currentY);
+                ++currentY;
+            }
+            ++currentX;
+        }
+
+
+        // -13.5 <-> 13.5   27 (-13.5)
+        // 15 <-> -15       30 (-15)
+    }
+
+    protected void InitializeGridTypes () {
+        // Corridors
+        for (int i = 0; i < c_GridWidth; ++i) {
+            for (int j = 0; j < c_GridHeight; ++j) {
+                Vector2 tilePosition = m_Grid[i][j].GetPosition ();
+                if (tilePosition.y == 14.0f) {
+                    if (tilePosition.x >= -12.5f
+                        && tilePosition.x <= -1.5f) {
+                        m_Grid[i][j].p_Type = TILE_TYPES.CORRIDOR;
+                    }
+                    else if (tilePosition.x >= 1.5f
+                        && tilePosition.x <= 12.5f) {
+                        m_Grid[i][j].p_Type = TILE_TYPES.CORRIDOR;
+                    }
+                }
+                else if (tilePosition.y == 10.0f) {
+                    if (tilePosition.x >= -12.5f
+                        && tilePosition.x <= 12.5f) {
+                        m_Grid[i][j].p_Type = TILE_TYPES.CORRIDOR;
+                    }
+                }
+                else if (tilePosition.y == 7.0f) {
+                    if (tilePosition.x >= -12.5f
+                        && tilePosition.x <= -7.5f) {
+                        m_Grid[i][j].p_Type = TILE_TYPES.CORRIDOR;
+                    }
+                    else if (tilePosition.x >= -4.5f
+                        && tilePosition.x <= -1.5f) {
+                        m_Grid[i][j].p_Type = TILE_TYPES.CORRIDOR;
+                    }
+                    else if (tilePosition.x >= 1.5f
+                        && tilePosition.x <= 4.5f) {
+                        m_Grid[i][j].p_Type = TILE_TYPES.CORRIDOR;
+                    }
+                    else if (tilePosition.x >= 7.5f
+                        && tilePosition.x <= 12.5f) {
+                        m_Grid[i][j].p_Type = TILE_TYPES.CORRIDOR;
+                    }
+                }
+                else if (tilePosition.y == 4.0f) {
+                    if (tilePosition.x >= -4.5f
+                        && tilePosition.x <= 4.5f) {
+                        m_Grid[i][j].p_Type = TILE_TYPES.CORRIDOR;
+                    }
+                }
+                else if (tilePosition.y == 1.0f) {
+                    if (tilePosition.x >= -12.5f
+                        && tilePosition.x <= -4.5f) {
+                        m_Grid[i][j].p_Type = TILE_TYPES.CORRIDOR;
+                    }
+                    else if (tilePosition.x >= 4.5f
+                        && tilePosition.x <= 12.5f) {
+                        m_Grid[i][j].p_Type = TILE_TYPES.CORRIDOR;
+                    }
+                }
+                else if (tilePosition.y == -2.0f) {
+                    if (tilePosition.x >= -4.5f
+                        && tilePosition.x <= 4.5f) {
+                        m_Grid[i][j].p_Type = TILE_TYPES.CORRIDOR;
+                    }
+                }
+                else if (tilePosition.y == -5.0f) {
+                    if (tilePosition.x >= -12.5f
+                        && tilePosition.x <= -1.5f) {
+                        m_Grid[i][j].p_Type = TILE_TYPES.CORRIDOR;
+                    }
+                    else if (tilePosition.x >= 1.5f
+                        && tilePosition.x <= 12.5f) {
+                        m_Grid[i][j].p_Type = TILE_TYPES.CORRIDOR;
+                    }
+                }
+                else if (tilePosition.y == -8.0f) {
+                    if (tilePosition.x >= -12.5f
+                        && tilePosition.x <= -10.5f) {
+                        m_Grid[i][j].p_Type = TILE_TYPES.CORRIDOR;
+                    }
+                    else if (tilePosition.x >= -7.5f
+                        && tilePosition.x <= 7.5f) {
+                        m_Grid[i][j].p_Type = TILE_TYPES.CORRIDOR;
+                    }
+                    else if (tilePosition.x >= 10.5f
+                        && tilePosition.x <= 12.5f) {
+                        m_Grid[i][j].p_Type = TILE_TYPES.CORRIDOR;
+                    }
+                }
+                else if (tilePosition.y == -11.0f) {
+                    if (tilePosition.x >= -12.5f
+                        && tilePosition.x <= -7.5f) {
+                        m_Grid[i][j].p_Type = TILE_TYPES.CORRIDOR;
+                    }
+                    else if (tilePosition.x >= -4.5f
+                        && tilePosition.x <= -1.5f) {
+                        m_Grid[i][j].p_Type = TILE_TYPES.CORRIDOR;
+                    }
+                    else if (tilePosition.x >= 1.5f
+                        && tilePosition.x <= 4.5f) {
+                        m_Grid[i][j].p_Type = TILE_TYPES.CORRIDOR;
+                    }
+                    else if (tilePosition.x >= 7.5f
+                        && tilePosition.x <= 12.5f) {
+                        m_Grid[i][j].p_Type = TILE_TYPES.CORRIDOR;
+                    }
+                }
+                else if (tilePosition.y == -14.0f) {
+                    if (tilePosition.x >= -12.5f
+                        && tilePosition.x <= 12.5f) {
+                        m_Grid[i][j].p_Type = TILE_TYPES.CORRIDOR;
+                    }
+                }
+            }
+        }
+
+        // Spawns
+        for (int i = 0; i < c_GridWidth; ++i) {
+            for (int j = 0; j < c_GridHeight; ++j) {
+            }
+        }
+
+        // Goal
+        for (int i = 0; i < c_GridWidth; ++i) {
+            for (int j = 0; j < c_GridHeight; ++j) {
+            }
+        }
     }
 }
