102bc11e62eae4cee80ee93714b24dca52aa46d2
Adrien Peyromaure Debord Broca <a.peyromauredebordbroca@rubika-edu.com>
2015-01-15 14:14:36 +0100

    Does TileGridding

--- Assets/Scripts/TileGridScript.cs
+++ Assets/Scripts/TileGridScript.cs
@@ -12,8 +12,12 @@ public class TileGridScript : MonoBehaviour {
         InitializeGrid ();
     }
 
-    protected void Update () {
-
+    protected void Start () {
+        for (int i = 0; i < c_GridWidth; ++i) {
+            for (int j = 0; j < c_GridHeight; ++j) {
+                Debug.Log (m_Grid[i][j].p_Type.ToString ());
+            }
+        }
     }
 
     protected void InitializeGrid () {
@@ -32,16 +36,16 @@ public class TileGridScript : MonoBehaviour {
             ++currentX;
         }
 
-
-        // -13.5 <-> 13.5   27 (-13.5)
-        // 15 <-> -15       30 (-15)
+        InitializeGridTypes ();
     }
 
     protected void InitializeGridTypes () {
-        // Corridors
+        #region Corridors
         for (int i = 0; i < c_GridWidth; ++i) {
             for (int j = 0; j < c_GridHeight; ++j) {
                 Vector2 tilePosition = m_Grid[i][j].GetPosition ();
+
+                #region Rows
                 if (tilePosition.y == 14.0f) {
                     if (tilePosition.x >= -12.5f
                         && tilePosition.x <= -1.5f) {
@@ -146,19 +150,204 @@ public class TileGridScript : MonoBehaviour {
                         m_Grid[i][j].p_Type = TILE_TYPES.CORRIDOR;
                     }
                 }
+                #endregion
+
+                #region Columns
+                if (tilePosition.x == -12.5f) {
+                    if (tilePosition.y >= -14.0f
+                        && tilePosition.y <= -11.0f) {
+                        m_Grid[i][j].p_Type = TILE_TYPES.CORRIDOR;
+                    }
+                    else if (tilePosition.y >= -8.0f
+                        && tilePosition.y <= -5.0f) {
+                        m_Grid[i][j].p_Type = TILE_TYPES.CORRIDOR;
+                    }
+                    else if (tilePosition.y >= 7.0f
+                        && tilePosition.y <= 14.0f) {
+                        m_Grid[i][j].p_Type = TILE_TYPES.CORRIDOR;
+                    }
+                }
+                else if (tilePosition.x == -10.5f) {
+                    if (tilePosition.y >= -11.0f
+                        && tilePosition.y <= -8.0f) {
+                        m_Grid[i][j].p_Type = TILE_TYPES.CORRIDOR;
+                    }
+                }
+                else if (tilePosition.x == -7.5f) {
+                    if (tilePosition.y >= -11.0f
+                        && tilePosition.y <= 14.0f) {
+                        m_Grid[i][j].p_Type = TILE_TYPES.CORRIDOR;
+                    }
+                }
+                else if (tilePosition.x == -4.5f) {
+                    if (tilePosition.y >= -11.0f
+                        && tilePosition.y <= -8.0f) {
+                        m_Grid[i][j].p_Type = TILE_TYPES.CORRIDOR;
+                    }
+                    else if (tilePosition.y >= -5.0f
+                        && tilePosition.y <= 4.0f) {
+                        m_Grid[i][j].p_Type = TILE_TYPES.CORRIDOR;
+                    }
+                    else if (tilePosition.y >= 7.0f
+                        && tilePosition.y <= 10.0f) {
+                        m_Grid[i][j].p_Type = TILE_TYPES.CORRIDOR;
+                    }
+                }
+                else if (tilePosition.x == -1.5f) {
+                    if (tilePosition.y >= -14.0f
+                        && tilePosition.y <= -11.0f) {
+                        m_Grid[i][j].p_Type = TILE_TYPES.CORRIDOR;
+                    }
+                    else if (tilePosition.y >= -8.0f
+                        && tilePosition.y <= -5.0f) {
+                        m_Grid[i][j].p_Type = TILE_TYPES.CORRIDOR;
+                    }
+                    else if (tilePosition.y >= 4.0f
+                        && tilePosition.y <= 7.0f) {
+                        m_Grid[i][j].p_Type = TILE_TYPES.CORRIDOR;
+                    }
+                    else if (tilePosition.y >= 10.0f
+                        && tilePosition.y <= 14.0f) {
+                        m_Grid[i][j].p_Type = TILE_TYPES.CORRIDOR;
+                    }
+                }
+                else if (tilePosition.x == 1.5f) {
+                    if (tilePosition.y >= -14.0f
+                        && tilePosition.y <= -11.0f) {
+                        m_Grid[i][j].p_Type = TILE_TYPES.CORRIDOR;
+                    }
+                    else if (tilePosition.y >= -8.0f
+                        && tilePosition.y <= -5.0f) {
+                        m_Grid[i][j].p_Type = TILE_TYPES.CORRIDOR;
+                    }
+                    else if (tilePosition.y >= 4.0f
+                        && tilePosition.y <= 7.0f) {
+                        m_Grid[i][j].p_Type = TILE_TYPES.CORRIDOR;
+                    }
+                    else if (tilePosition.y >= 10.0f
+                        && tilePosition.y <= 14.0f) {
+                        m_Grid[i][j].p_Type = TILE_TYPES.CORRIDOR;
+                    }
+                }
+                else if (tilePosition.x == 4.5f) {
+                    if (tilePosition.y >= -11.0f
+                        && tilePosition.y <= -8.0f) {
+                        m_Grid[i][j].p_Type = TILE_TYPES.CORRIDOR;
+                    }
+                    else if (tilePosition.y >= -5.0f
+                        && tilePosition.y <= 4.0f) {
+                        m_Grid[i][j].p_Type = TILE_TYPES.CORRIDOR;
+                    }
+                    else if (tilePosition.y >= 7.0f
+                        && tilePosition.y <= 10.0f) {
+                        m_Grid[i][j].p_Type = TILE_TYPES.CORRIDOR;
+                    }
+                }
+                else if (tilePosition.x == 7.5f) {
+                    if (tilePosition.y >= -11.0f
+                        && tilePosition.y <= 14.0f) {
+                        m_Grid[i][j].p_Type = TILE_TYPES.CORRIDOR;
+                    }
+                }
+                else if (tilePosition.x == 10.5f) {
+                    if (tilePosition.y >= -11.0f
+                        && tilePosition.y <= -8.0f) {
+                        m_Grid[i][j].p_Type = TILE_TYPES.CORRIDOR;
+                    }
+                }
+                else if (tilePosition.x == 12.5f) {
+                    if (tilePosition.y >= -14.0f
+                        && tilePosition.y <= -11.0f) {
+                        m_Grid[i][j].p_Type = TILE_TYPES.CORRIDOR;
+                    }
+                    else if (tilePosition.y >= -8.0f
+                        && tilePosition.y <= -5.0f) {
+                        m_Grid[i][j].p_Type = TILE_TYPES.CORRIDOR;
+                    }
+                    else if (tilePosition.y >= 7.0f
+                        && tilePosition.y <= 14.0f) {
+                        m_Grid[i][j].p_Type = TILE_TYPES.CORRIDOR;
+                    }
+                }
+                #endregion
             }
         }
+        #endregion
 
-        // Spawns
+        #region Spawns
         for (int i = 0; i < c_GridWidth; ++i) {
             for (int j = 0; j < c_GridHeight; ++j) {
+                Vector2 tilePosition = m_Grid[i][j].GetPosition ();
+                if (tilePosition.y == 15.0f) {
+                    if (tilePosition.x >= -13.5f
+                        && tilePosition.x <= -11.5f) {
+                        m_Grid[i][j].p_Type = TILE_TYPES.SPAWN;
+                    }
+                }
+                else if (tilePosition.y == 14.0f) {
+                    if (tilePosition.x >= -13.5f
+                        && tilePosition.x <= -11.5f) {
+                        m_Grid[i][j].p_Type = TILE_TYPES.SPAWN;
+                    }
+                }
+                else if (tilePosition.y == 13.0f) {
+                    if (tilePosition.x >= -13.5f
+                        && tilePosition.x <= -12.5f) {
+                        m_Grid[i][j].p_Type = TILE_TYPES.SPAWN;
+                    }
+                }
+                else if (tilePosition.y == -13.0f) {
+                    if (tilePosition.x >= 12.5f
+                        && tilePosition.x <= 13.5f) {
+                        m_Grid[i][j].p_Type = TILE_TYPES.SPAWN;
+                    }
+                }
+                else if (tilePosition.y == -14.0f) {
+                    if (tilePosition.x >= 11.5f
+                        && tilePosition.x <= 13.5f) {
+                        m_Grid[i][j].p_Type = TILE_TYPES.SPAWN;
+                    }
+                }
+                else if (tilePosition.y == -15.0f) {
+                    if (tilePosition.x >= 11.5f
+                        && tilePosition.x <= 13.5f) {
+                        m_Grid[i][j].p_Type = TILE_TYPES.SPAWN;
+                    }
+                }
             }
         }
+        #endregion
 
-        // Goal
+        #region Goal
         for (int i = 0; i < c_GridWidth; ++i) {
             for (int j = 0; j < c_GridHeight; ++j) {
+                Vector2 tilePosition = m_Grid[i][j].GetPosition ();
+                if (tilePosition.y == 3.0f) {
+                    if (tilePosition.x >= -0.5f
+                        && tilePosition.x <= 0.5f) {
+                        m_Grid[i][j].p_Type = TILE_TYPES.GOAL;
+                    }
+                }
+                else if (tilePosition.y == 2.0f) {
+                    if (tilePosition.x >= -2.5f
+                        && tilePosition.x <= 2.5f) {
+                        m_Grid[i][j].p_Type = TILE_TYPES.GOAL;
+                    }
+                }
+                else if (tilePosition.y == 1.0f) {
+                    if (tilePosition.x >= -2.5f
+                        && tilePosition.x <= 2.5f) {
+                        m_Grid[i][j].p_Type = TILE_TYPES.GOAL;
+                    }
+                }
+                else if (tilePosition.y == 0.0f) {
+                    if (tilePosition.x >= -2.5f
+                        && tilePosition.x <= 2.5f) {
+                        m_Grid[i][j].p_Type = TILE_TYPES.GOAL;
+                    }
+                }
             }
         }
+        #endregion
     }
 }
