c2129108b55ceceda77e062edddbf357ba7dd68f
Adrien Peyromaure Debord Broca <a.peyromauredebordbroca@rubika-edu.com>
2015-01-15 10:42:56 +0100

    Adds collectibles

---
+++ Assets/Scripts/ResourcesManagmentScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ResourcesManagmentScript : MonoBehaviour {
+    #region Members
+    public int m_CurrentGold;
+    public int p_CurrentGold {
+        get {
+            return m_CurrentGold;
+        }
+        set {
+            m_CurrentGold = value;
+            Debug.Log (m_CurrentGold.ToString ());
+        }
+    }
+    public const int c_StartGold = 100;
+    #endregion
+
+    void Awake () {
+        p_CurrentGold = c_StartGold;
+    }
+}
