aaa541807529bd7456e74fc9e1b97941d7262b3e
Mathieu Bédu <mathieu.bedu@hotmail.fr>
2015-01-16 10:29:45 +0100

    Ajout de la GUI

---
+++ Assets/Scripts/GUIScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GUIScript : MonoBehaviour {
+    private ResourcesManagmentScript resourcesManagmentScript;
+
+    void FixedUpdate () {
+        // Màj vie
+        transform.GetChild (0).GetComponent<Text> ().text = "Gums : " + resourcesManagmentScript.p_CurrentLifePoint;
+
+        // Màj gums
+        transform.GetChild (1).GetComponent<Text> ().text = "Gums : " + resourcesManagmentScript.p_CurrentGold;
+    }
+
+    void Start () {
+        resourcesManagmentScript = GameObject.Find ("ResourcesManager").GetComponent<ResourcesManagmentScript> ();
+    }
+}
