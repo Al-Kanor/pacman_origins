d2f3106409135a00d48de219ebd1b4e12931ff8a
Mathieu Bédu <mathieu.bedu@hotmail.fr>
2015-01-16 12:19:39 +0100

    Maj Scene

--- Assets/Scripts/GoalScript.cs
+++ Assets/Scripts/GoalScript.cs
@@ -2,11 +2,8 @@
 using System.Collections;
 
 public class GoalScript : MonoBehaviour {
-    public GameObject GoalFX;
-
     void OnTriggerEnter (Collider other) {
         if ("Ghost" == other.tag) {
-            GoalFX.GetComponent<ParticleSystem> ().Play ();
             other.gameObject.GetComponent<GhostScript> ().Death (goalReached: true);
             GameObject.Destroy (other.gameObject);
             GameObject.Destroy (this.transform.FindChild ("Fruits").GetChild (0).gameObject);
